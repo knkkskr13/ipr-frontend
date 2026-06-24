793cf49d65d0d47bbfbed54b63a46470eaf3e5ae
knkkskr13 <knkkskr@gmail.com>
2026-06-24 18:45:03 +0530

    fix: update API base URL to Render backend

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://ipr-backend-cej0.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 });
 
